d87c6ba2351574a966a7f4c2dce0ff75f0ba1cb6
naqashsaeed <110665986+naqashsaeed@users.noreply.github.com>
2022-08-09 22:57:17 +0500

    Add files via upload

---
+++ weknow-it.co.uk/developer/we_know/ai_ch.php
@@ -0,0 +1,80 @@
+
+
+<?php include('header.php') ?>
+
+<section class="backg white">
+  <div >
+<img src="./images/ai.jpg" width="100%" height="400px;">
+  </div>
+</section>
+<div class="white">
+
+  <section>
+
+      <ul class="breadcrumb">
+        <li><a href="index.php">Home</a></li>
+        <li>AI &amp; ChatBot</li>
+      </ul>
+
+    <div><h1>Why to have a ChatBot</h1></div>
+    <div><p>Stay ahead of your competition in terms of customer service, availability, and convenience, by custom-building an intelligent chatbot with our developers.</p></div>
+  </section>
+</div>
+
+
+<div class="green">
+<br>
+  <section>
+    <div>
+      <h1>Custom Stunning ChatBot</h1>
+    </div>
+    <div><p>You aren’t getting a secondhand program rolled out as a kind of one-size-fits
+      all solution; you’re getting a custom, personalized chatbot designed just for you and
+       your customers. We’ll develop anything, including chatbots for e-commerce, hospitality
+       and travel services, retail, marketing, banking, and much more.</p></div>
+  </section>
+
+</div>
+<div class="white">
+  <section>
+    <div>
+      <h1>Our Chatbot Can</h1>
+    </div>
+    <div>
+      <ul>
+        <li>Manage your business’s supply chains and generate purchases</li>
+        <li>Help you analyze market trends by remembering customer habits</li>
+        <li>Become a virtual assistant to you, your teams, or your customers</li>
+        <li>Provide constant customer service by answering questions about your company, your inventory, or the benefits you provide.</li>
+      </ul>
+    </div>
+    <div>
+      <h1>The options are unlimited,<br>
+         because we tailor each bot to your specific needs.</h1>
+    </div>
+    <div><p>By using your chatbot, your customers will have an easy way to get
+       the information they need, no matter what service you provide. No more call centers.
+       No more wait times. No more navigating difficult websites. They’ll love you and your business.</p></div>
+     </section>
+
+   </div>
+<div>
+  <section class="green">
+    <div>
+      <h1></h1>
+    </div>
+    <div><h2></h2></div>
+    <div><p></p></div>
+  </section>
+</div>
+<section class="white">
+
+  <div class="tech_btn">
+<br><br>
+    <a href="start.php"><button class="c-btn1"> Contact Us for ChatBot > ></button></a></div>
+    </div>
+<br><br><br>
+
+</section>
+<?php include('choose.php') ?>
+<?php include('footer.php') ?>
